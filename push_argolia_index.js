6406cf407dbde24d743d3ebcd6d70e7a72499f08
ryan <xiaoyin_c@qq.com>
2021-01-22 23:26:54 +0800

    docs: fix search

---
+++ push_argolia_index.js
@@ -0,0 +1,22 @@
+/*
+上传 algolia 索引文件
+参考：https://www.algolia.com/doc/guides/getting-started/quick-start/tutorials/quick-start-with-the-api-client/javascript/?client=javascript
+*/
+
+// For the default version
+const algoliasearch = require('algoliasearch');
+
+const appID = "747LJ10EI7"
+const indexName = "ryan-space"
+const adminKey = process.env.ALGOLIA_ADMIN_KEY
+const indexFile = "./public/index.json"
+
+const client = algoliasearch(appID, adminKey);
+const index = client.initIndex(indexName);
+const indexJson = require(indexFile);
+
+index.saveObjects(indexJson, {
+  autoGenerateObjectIDIfNotExist: true
+}).then(({ objectIDs }) => {
+  console.log(objectIDs);
+});
